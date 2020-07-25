95b630acda9ba710080ef9f00fbd18f323fd603a
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-25 23:09:13 +0700

    Second Commit

---
+++ 11.js
@@ -0,0 +1,10 @@
+//Percabangan Bentuk 1
+
+//mengisi data numerik pada variabel tahun
+let tahun = 2020
+
+//if, kode hanya dieksekusi bila nilainya true
+//2020 bila dibagi 4 sisanya akan 0 maka nilainya true sesuai statement
+if(tahun % 4 == 0){
+    console.log(tahun + " adalah tahun kabisat");
+}
